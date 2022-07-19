ced765caa87598bda2105e406616d43b5ed976f0
Azarill <heierms@gmail.com>
2022-07-19 14:56:52 -0500

    added navbar and about components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+// these will be the different headings for the webpage navigation
+const categories = [
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ];
+
+  function categorySelected(name) {
+    console.log(`${name} clicked`)
+  }
+
+  // returns navigation bar code
+function Nav(){
+    return(
+        <header>
+        <h2>
+          <a href="/">
+            <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          </a>
+        </h2>
+    {/*  This is the main components of nav bar */}
+        <nav>
+          <ul className="flex-row">
+            <li className="mx-2">
+              <a href="#about">
+                About me
+              </a>
+            </li>
+            <li>
+              <span>Contact</span>
+            </li>
+        {/*  This is the nav buttons getting mapped out */}
+            {categories.map((category) => (
+              <li className="mx-1" key={category.name}> <span onClick={() => categorySelected(category.name)} >{category.name}</span></li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    );
+};
+
+export default Nav;
