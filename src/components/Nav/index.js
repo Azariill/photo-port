cfd1cab4ecf4869546a48556a516cae45fa7b670
Azarill <heierms@gmail.com>
2022-07-23 06:11:11 -0500

    implemented hooks for navigation

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,52 +1,57 @@
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import React, {useEffect} from 'react';
 
-// these will be the different headings for the webpage navigation
-const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ];
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
 
-  function categorySelected(name) {
-    console.log(`${name} clicked`)
-  }
+  // the second argument currentCategory tells the browser to re-render if the currentCategory changes
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
 
-  // returns navigation bar code
-function Nav(){
-    return(
-        <header>
-        <h2>
-            <a data-testid="link" href="/">
-                <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+  }, [currentCategory]);
+
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about">
+              About me
             </a>
-        </h2>
-    {/*  This is the main components of nav bar */}
-        <nav>
-          <ul className="flex-row">
-            <li className="mx-2">
-              <a data-testid="about" href="#about">
-                About me
-              </a>
+          </li>
+          <li className="mx-2">
+            <span>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
             </li>
-            <li>
-              <span>Contact</span>
-            </li>
-        {/*  This is the nav buttons getting mapped out */}
-            {categories.map((category) => (
-              <li className="mx-1" key={category.name}> <span onClick={() => categorySelected(category.name)} >{category.name}</span></li>
-            ))}
-          </ul>
-        </nav>
-      </header>
-    );
-};
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
 export default Nav;
